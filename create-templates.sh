d8bcd2f4f4980767b8f2ab0422761f993c506fe9
Alec Smecher <alec@smecher.bc.ca>
2023-01-06 15:39:43 -0800

    Initial commit

--- create-templates.sh
+++ create-templates.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # Determine which base directories contain locale files for which templates must be created.
 BASEDIRS=`git ls-files | grep -e "\/en_US\/[a-zA-Z]\+.po$" | xargs dirname | xargs dirname | sort | uniq`
 for BASEDIR in $BASEDIRS; do
 	mkdir -p $BASEDIR/templates
 	for FILE in $BASEDIR/en_US/*.po; do
 		FILENAME=`basename $FILE`
-		echo $FILE
 		msgfilter -i $FILE -o $BASEDIR/templates/${FILENAME}t true
 	done
 done

a95951a2b5d51e97b35f6e5f90ba7297dd524183
Alec Smecher <alec@smecher.bc.ca>
2023-02-02 16:58:10 -0800

    Add usage information

--- scatter-locales.sh
+++ scatter-locales.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+	echo "Usage: $0 [REPO]"
+	echo "	Scatter the locale files from the summit into the repository REPO."
+	echo ""
+	echo "  REPO is expected to be in one directory above this toolset in the directory tree."
+	echo "  See https://github.com/asmecher/summit-tools for more information."
+	exit 1
+fi
+
 REPO=$1
 
 for base in `sh list-bases.sh ../${REPO}/main`; do
